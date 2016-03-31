3a28a2184475e8140f9f00b9afeb2416818bbd9a
gologuzov <gologuzov@bifit.com>
2016-04-01 00:57:03 +0300

    Added Stream to module export

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ module.exports = {
     AdapterTLS: require('./lib/adapter_tls'),
     Connect: require('./lib/connect'),
     ProtoMessages: require('./lib/proto_messages')
+    Stream: require('./lib/stream')
 }
